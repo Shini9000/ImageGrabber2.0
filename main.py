4c414aa66961d54cbbdb5f73df221a6d277501e0
Aidan Christie <aidanchristie98@gmail.com>
2024-03-11 12:05:07 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+import requests
+from PIL import Image
+
+
+selectlist = ['Hedgehog', 'Dog', 'Cat', 'Goat', 'Cow', 'Fish', 'Highlandcow', 'Pig', 'Duck', 'Piglet','Exit']
+
+def Image_Selector():
+    print(f'Select from the list: \n {selectlist}. \nor enter your own input.')
+
+    ans = input()
+
+    if 'Close' in ans or 'Exit' in ans:
+        exit()
+    else:
+        print(f"Please wait whilst we load a image of {ans.title()}")
+        image_generator(ans)
+
+def image_generator(txt):
+    response = requests.get(
+        "https://source.unsplash.com/1920x1080?{0}".format(txt)
+        )
+    file = open('image.jpg', 'wb')
+    file.write(response.content)
+    img = Image.open(r"image.jpg")
+    img.show()
+    file.close()
+    pass
+
+
+Image_Selector()
